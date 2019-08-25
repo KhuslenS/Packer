19b1fbf2c659afcd8ec2e5de5c91967a6f6ec001
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-25 16:05:25 -0500

    file is updated

--- JenkinsfileUpdate.groovy
+++ JenkinsfileUpdate.groovy
@@ -1,8 +1,79 @@
-node {
-    stage("Pull Repo"){
-        git "https://github.com/Khuslentuguldur/Packer.git"
+pipeline{
+    agent any
+    stages{
+        stage("Run Command"){
+            steps{
+              sh '''
+              set +xe
+              echo Hello
+              sudo yum  install httpd wget unzip -y
+              '''
+            }
+        }
+        stage("Download Terraform"){
+            steps{
+                ws("tmp/"){
+                    script {
+                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                            sh "terraform version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                        }
+                    }
+                }
+            }
+        }
+        stage("write to a file"){
+            steps{
+                ws("tmp/"){
+                    writeFile text: "Test", file: "Testfile"
+                }
+            }
+        }
+        stage("Download Packer"){
+            steps{
+                ws("tmp/"){
+                    script {
+                        def exists = fileExists 'packer_1.4.3_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /sbin"
+                            sh "packer version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip"
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /sbin"
+                            sh "packer version"
+                        }
+                    }
+                }
+            }
+        }
+        stage("Pull Repo"){
+            steps{
+                git("https://github.com/Khuslentuguldur/Packer.git")
+                sh "ls"
+            }
+        }
+        stage("Build Image"){
+            steps{
+                // sh "packer build updated/updated.json"
+                sh "ls"
+            }
+        }
+
     }
-    stage("Build AMI"){
-        sh "packer build updated/updated.json"
+    post{
+      success {
+          echo "Done"
+        }
+        failure {
+          mail to:  "khuslen-cyber@gmail.com", subject: “job”, body: "job completed"
+        }
+      }
     }
-}
